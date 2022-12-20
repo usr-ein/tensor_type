9c9d2338a5695e31a679e7d0f55118111d602597
Richard D. Paul <richard@los-paul.eu>
2022-12-20 15:09:25 +0100

    Add improved slice support for shape ranges.
    So far slices were always treated as if being `slice(None, None, None)`.
    Changed this to support proper ranges, like e.g. `Tensor[:,1:3,:,:]`,
    which now accepts
    4D Tensors with either one or two entries along the second dimension.

--- tensor_type/tensor_type.py
+++ tensor_type/tensor_type.py
@@ -82,8 +82,11 @@ class _Tensor:
 
         for real_dim, type_dim in zip(x.shape, cls.type_shape):
             if isinstance(type_dim, slice):
-                continue
-            valid &= real_dim == type_dim
+                valid &= type_dim.start is None or real_dim >= type_dim.start       # check if within bounds ...
+                valid &= type_dim.stop is None  or real_dim < type_dim.stop         # excluding stop, as usual
+                valid &= type_dim.step is None  or real_dim % type_dim.step == 0    # within start:stop:step 
+            else:
+                valid &= real_dim == type_dim
 
         return valid
 
